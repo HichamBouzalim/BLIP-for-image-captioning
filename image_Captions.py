0708ef315508e2ca507a51793342b697696400cd
Hicham Bouzalim <bouzalimhicham11@gmail.com>
2025-09-10 18:58:17 +0200

    updated

--- image_Captions.py
+++ image_Captions.py
@@ -1,15 +1,22 @@
-# Install the transformers library
 from transformers import BlipProcessor, BlipForConditionalGeneration
 from PIL import Image
-# Initialize the processor and model from Hugging Face
+
+# Initialize the processor from Hugging Face
 processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+
+# Initialize the model from Hugging Face
 model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+
+
 # Load an image
-image = Image.open("C:\Users\Sniper\AI Project\Coursera\IBM\BLIP for image captioning\Picture.jpg")
+image = Image.open("Picture.jpg").convert("RGB")
+
 # Prepare the image
 inputs = processor(image, return_tensors="pt")
+
 # Generate captions
-outputs = model.generate(**inputs)
-caption = processor.decode(outputs[0],skip_special_tokens=True)
- 
+outputs_ids = model.generate(**inputs)
+caption = processor.decode(outputs_ids[0],skip_special_tokens=True)
+
+
 print("Generated Caption:", caption)
